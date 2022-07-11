e26b8bdc1ba30d8e751cc2e969744769df3eb2fa
syedfaisalsaleeem <syedfaisalsaleem.100@gmail.com>
2022-07-11 23:55:00 +0500

    updated python test.py file

--- test.py
+++ test.py
@@ -1,18 +1,11 @@
 ## test graphql
 
-import load_env
 import asyncio
+from tests import load_test_env
 import pytest
-from main import application as app
 from populate import create_tables
-from tests.graphql.db import overide_get_session, engine
-from src.graphql.db.session import get_session
-
-app.dependency_overrides[get_session] = overide_get_session
+from tests.graphql.db import engine 
 
 if __name__ == "__main__":
-    print("Populating database...")
     asyncio.run(create_tables(engine))
-    print("Database populated.")
-    
-    pytest.main(args=[ "--cov=tests","tests/graphql","-s","--disable-warnings"])
+    pytest.main(args=[ "--cov=tests","tests/graphql/test_user_stickynotes.py","-s","--disable-warnings"])
